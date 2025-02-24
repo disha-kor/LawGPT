2d96588337556b85a163f916d1bbeaeb727a383d
disha-kor <kushinakor@gmail.com>
2025-02-24 20:48:35 +0530

    added navbar and hero sections

---
+++ app/(front)/layout.tsx
@@ -0,0 +1,12 @@
+import React, {ReactNode} from 'react'
+import Navbar from '@/components/Frontend/Navbar'
+
+export default function Layout({children}:{children:ReactNode}) {
+  return (
+    <div>
+        <Navbar/>
+        {children}
+    </div>
+  )
+}
+ 
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Home() {
-  return (
-    <section className="text-3xl flex min-h-screen justify-center items-center">
-      <h2 className="font-semibold">LawGPT</h2>
-    </section>
-  );
-}
